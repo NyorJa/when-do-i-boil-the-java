aed0241e79aedb7d80d28806a424f93d6afe0dcc
Alexander Greene <greene.alexanderj@gmail.com>
2018-12-10 14:18:41 -0500

    update api mapping for deleting recipe to have it pull the recipe out of any meals that contain it before deleting the recipe

--- src/main/java/org/wecancodeit/whendoiboilthewater/repository/MealRepository.java
+++ src/main/java/org/wecancodeit/whendoiboilthewater/repository/MealRepository.java
@@ -1,8 +1,13 @@
 package org.wecancodeit.whendoiboilthewater.repository;
 
+import java.util.Collection;
+
 import org.springframework.data.repository.CrudRepository;
 import org.wecancodeit.whendoiboilthewater.model.Meal;
+import org.wecancodeit.whendoiboilthewater.model.Recipe;
 
 public interface MealRepository extends CrudRepository<Meal, Long> {
+
+	public Collection<Meal> findByRecipes(Recipe recipe);
 
 }
